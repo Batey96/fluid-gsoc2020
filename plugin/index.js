7978713daad0a875d61893d4d389dc1323aad5ea
batey96 <lordbatemartins@gmail.com>
2020-03-26 18:35:12 +0100

    Now plugins can be plugged into build process

--- plugin/index.js
+++ plugin/index.js
@@ -1,24 +1,6 @@
-var shell = require('shelljs');
-var config = require('../configs');
-var fetch = require('node-fetch');
-
-async function getSubModule(){
-   // fetch("https://api.github.com/repos/Batey96/infusion-docs/tags")
-   
-   const response =  await fetch('https://api.github.com/repos/Batey96/infusion-docs/tags')
-  .then(response => response.json())
-  .then(data => {
-    return data
-  })
-   // shell.exec("git submodule add https://github.com/Batey96/infusionDocsContents.git "+config.content_path+"docs");
-   response.forEach(element => {
-    shell.exec("echo Versioin "+element.name);
-});
-  }
-  
-function netlifyPlugin(conf) {
+  function netlifyPlugin(conf) {
   return {
-    name: 'netlify-plugin-a11y',
+    name: 'plugin',
     async onPostBuild() {
       console.log('Runing from above xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx');
     }
