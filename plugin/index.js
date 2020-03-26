b204d7ebb7e8fcd7cfa76f13c8a12d80d25dba67
batey96 <lordbatemartins@gmail.com>
2020-03-26 19:32:11 +0100

    submodule code

--- plugin/index.js
+++ plugin/index.js
@@ -3,7 +3,7 @@ var shell = require('shelljs');
   function netlifyPlugin(conf) {
   return {
     name: 'plugin',
-    async onPostBuild() {
+    async onInit() {
       shell.exec("git submodule add https://github.com/Batey96/infusionDocsContents.git docs");
     }
   };
