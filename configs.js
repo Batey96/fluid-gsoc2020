48043f019c61e709c310bad4b1ae7c23c20912b0
batey96 <lordbatemartins@gmail.com>
2020-03-26 09:15:28 +0100

    structuring again

---
+++ configs.js
@@ -0,0 +1,3 @@
+var content_path = '';  //Default directory where submodules will be added to. Configure it for your preference
+exports.content_path = content_path
+development = false
---
+++ plugins/scritpt.js
@@ -0,0 +1,21 @@
+var shell = require('shelljs');
+var config = require('../configs');
+var fetch = require('node-fetch');
+let versions = ['v1','v2','v3']
+let k = '';
+async function getSubModule(){
+   // fetch("https://api.github.com/repos/Batey96/infusion-docs/tags")
+   
+   const response =  await fetch('https://api.github.com/repos/Batey96/infusion-docs/tags')
+  .then(response => response.json())
+  .then(data => {
+    return data
+  })
+   // shell.exec("git submodule add https://github.com/Batey96/infusionDocsContents.git "+config.content_path+"docs");
+   response.forEach(element => {
+    shell.exec("echo Versioin "+element.name);
+});
+  }
+getSubModule().then(res=>{
+    console.log('yoopie all is done');
+})
